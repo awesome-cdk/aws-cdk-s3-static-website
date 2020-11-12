d779ae3434527f451e197d91fa0d0ca39a9e7cf6
Dzhuneyt Ahmed <dzhuneyt@dzhuneyt.com>
2020-11-12 14:37:14 +0200

    chore: Cleanup

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import {AwsCdkS3StaticWebsiteStack} from '../lib/aws-cdk-s3-static-website-stack';
+import {AwsCdkS3StaticWebsiteStack} from "./lib/aws-cdk-s3-static-website-stack";
 
 const app = new cdk.App();
 new AwsCdkS3StaticWebsiteStack(app, 'AwsCdkS3StaticWebsiteStack');
